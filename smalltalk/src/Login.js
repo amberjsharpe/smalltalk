b77e46a274c2d4a6e50d148e9194de8f1a134db0
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-08 11:20:30 -0500

    moved button to bottom

--- smalltalk/src/Login.js
+++ smalltalk/src/Login.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from './images/whitespeech.png';
 import './logo.css';
+import './button.css';
 
 class Login extends Component {
-    render() {
+    render(props) {
         return(
             <div>
                 <img className="largeLogo" src={logo} alt="smalltalk logo"/>
                 <h1 className="logoName">SmallTalk</h1>
                 <h2>Ready, set, let's chat!</h2>
+                <button className="stickToBottom longButton backgroundYellow">
+                    <strong>LOGIN</strong>
+                </button>
             </div>
         )
     }
