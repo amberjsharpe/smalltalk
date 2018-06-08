854ccd81a69e5b37325406c5e889059cbaa6e6b0
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-08 10:02:35 -0500

    added font and two headers for login

--- smalltalk/src/Login.js
+++ smalltalk/src/Login.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './images/whitespeech.png';
+import './logo.css';
 
 class Login extends Component {
     render() {
         return(
             <div>
-                <img src={logo}/>
+                <img className="largeLogo" src={logo} alt="smalltalk logo"/>
+                <h1 className="logoName">SmallTalk</h1>
+                <h2>Ready, set, let's chat!</h2>
             </div>
         )
     }
