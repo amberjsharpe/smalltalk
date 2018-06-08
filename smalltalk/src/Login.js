e01e569c36db7c32da772311ef73c7d4322269bf
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-08 11:57:38 -0500

    centered items and added padding

--- smalltalk/src/Login.js
+++ smalltalk/src/Login.js
@@ -4,15 +4,19 @@ import './logo.css';
 import './button.css';
 
 class Login extends Component {
-    render(props) {
+    render() {
         return(
             <div>
-                <img className="largeLogo" src={logo} alt="smalltalk logo"/>
-                <h1 className="logoName">SmallTalk</h1>
-                <h2>Ready, set, let's chat!</h2>
+                <div className="d-flex flex-column align-items-center">
+                    <div className="pt-5">
+                        <img className="largeLogo" src={logo} alt="smalltalk logo"/>
+                        <h1 className="logoName pb-5">SmallTalk</h1>
+                    </div>
+                    <h2 className="pt-5"><strong>Ready, set, let's chat!</strong></h2>
+                </div>
                 <button className="stickToBottom longButton backgroundYellow">
                     <strong>LOGIN</strong>
-                </button>
+                </button>   
             </div>
         )
     }
