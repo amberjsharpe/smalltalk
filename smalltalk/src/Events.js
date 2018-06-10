1568d831711fe000cf6e59be879309b35746ddab
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-09 21:09:20 -0500

    added props to event categories

--- smalltalk/src/Events.js
+++ smalltalk/src/Events.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Categorybtn from './Categorybtn';
 import party from './images/party.png';
+import champagne from './images/champagne.png';
+import coffee from './images/coffee.png';
+import family from './images/family.png';
+import network from './images/network.png';
+import train from './images/train.png';
 
 class Events extends Component {
     render() {
@@ -11,11 +16,11 @@ class Events extends Component {
                 <h3 className="pt-4 pl-3 pb-4">Where are you going?</h3>
                 <div className="d-flex flex-wrap">
                     <Categorybtn src={party} alt="party hat" category="Party"/>
-                    <Categorybtn src={party} alt="party hat" category="Party"/>
-                    <Categorybtn src={party} alt="party hat" category="Party"/>
-                    <Categorybtn src={party} alt="party hat" category="Party"/>
-                    <Categorybtn src={party} alt="party hat" category="Party"/>
-                    <Categorybtn src={party} alt="party hat" category="Party"/>
+                    <Categorybtn src={network} alt="circles connected by lines" category="Networking Event"/>
+                    <Categorybtn src={champagne} alt="champagne" category="Date"/>
+                    <Categorybtn src={family} alt="family" category="Family Gathering"/>
+                    <Categorybtn src={coffee} alt="coffee pot" category="Break Room"/>
+                    <Categorybtn src={train} alt="light rail train" category="Morning Commute"/>
                 </div>
             </div>
         )
