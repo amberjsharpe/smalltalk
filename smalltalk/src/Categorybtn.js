f213c4cb78397fd1d1bc8ab3a7fc93544b363556
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-09 20:44:57 -0500

    added box shadow and increased height

--- smalltalk/src/Categorybtn.js
+++ smalltalk/src/Categorybtn.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './categorybtn.css';
+import party from './images/party.png';
 
 class Categorybtn extends Component {
     render() {
         return(
-            <div>
-                <h4>Party</h4>
+            <div className="categorybtn boxShadow d-flex flex-column align-items-center">
+                <img src={party} alt="party hat"/>
+                <h3>Party</h3>
             </div>
         )
     }
