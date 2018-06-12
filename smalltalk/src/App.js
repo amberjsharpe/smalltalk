8b6e67407248ad9243eee097dc80faf014f8791b
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-12 12:02:55 -0500

    push to save progress

--- smalltalk/src/App.js
+++ smalltalk/src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import { 
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-import { googleProvider, rebase } from './base';
-import Home from './Home';
-import logo from './images/whitespeech.png';
+import Login from './Login';
+import User from './User';
 import './App.css';
 import './logo.css';
 import './button.css';
@@ -16,59 +10,23 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-
+   
     this.state = {
-        loggedin: ''
+      loggedin: '',
+      authed: false
     }
-
-    this.saveUser = this.saveUser.bind(this);
-    this.loginWithGoogle = this.loginWithGoogle.bind(this);
-  }
-
-  // Saves user to Firebase and sets state to logged in based on uid
-  saveUser (user) {
-      return rebase.initializedApp.database().ref().child(`${user.uid}/user`)
-      .update({
-          email: user.email,
-          uid: user.uid, 
-          name: user.displayName, 
-          image: user.photoURL
-      })
-      .then(() => {
-          this.setState({
-              loggedin: user.uid
-          });
-          console.log(this.state)
-      })
-  }
-
-  // Logins into app using Google Popup sign-in and renders saveUser() with user info 
-  loginWithGoogle() {
-  return rebase.initializedApp.auth().signInWithPopup(googleProvider)
-  .then((data) => {
-    this.saveUser(data.user);
-  })
   }
 
   render() {
-    return (
-      <Router>
-        <div className="App">
-          <div>
-            <div className="d-flex flex-column align-items-center">
-              <div className="login pt-5">
-                <img src={logo} alt="smalltalk logo"/>
-                <h1 className="logoName pb-5">SmallTalk</h1>
-              </div>
-                <h3 className="pt-5">Ready, set, let's chat!</h3>
-              </div>
-              <button onClick={this.loginWithGoogle} className="stickToBottom longBtn backgroundYellow">
-                <h2>LOGIN</h2>
-              </button>
+    console.log(this.state.authed === true)
+
+    if (this.state.authed === false) {
+         return (
+          <div> 
+            <Login loggedin={this.state.loggedin} authed={this.state.authed}/>
           </div>
-        </div>
-      </Router>
-    );
+      )
+    } 
   }
 }
 
