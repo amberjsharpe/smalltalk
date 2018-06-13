255e0f33afe1dede350f9ab238519fc5a379b21a
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-13 10:25:06 -0500

    events

---
+++ smalltalk/src/events/Commute.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Commute extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Commute
---
+++ smalltalk/src/events/Party.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Party extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Party
